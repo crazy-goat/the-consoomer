1a26264b8e050cb9fe3e99d36664ac30b81b6e0c
Piotr Hałas <piotr.h@decodo.com>
2026-04-20 20:10:44 +0200

    Remove redundant method_exists() tests from AmqpFactoryTest
    These tests are unnecessary because AmqpFactoryInterface already enforces
    method existence at compile time.

--- tests/Unit/AmqpFactoryTest.php
+++ tests/Unit/AmqpFactoryTest.php
@@ -9,36 +9,6 @@ use PHPUnit\Framework\TestCase;
 
 class AmqpFactoryTest extends TestCase
 {
-    public function testFactoryHasCreateConnectionMethod(): void
-    {
-        $factory = new AmqpFactory();
-        $this->assertTrue(method_exists($factory, 'createConnection'));
-    }
-
-    public function testFactoryHasCreateChannelMethod(): void
-    {
-        $factory = new AmqpFactory();
-        $this->assertTrue(method_exists($factory, 'createChannel'));
-    }
-
-    public function testFactoryHasCreateQueueMethod(): void
-    {
-        $factory = new AmqpFactory();
-        $this->assertTrue(method_exists($factory, 'createQueue'));
-    }
-
-    public function testFactoryHasCreateExchangeMethod(): void
-    {
-        $factory = new AmqpFactory();
-        $this->assertTrue(method_exists($factory, 'createExchange'));
-    }
-
-    public function testFactoryHasConfigureSslMethod(): void
-    {
-        $factory = new AmqpFactory();
-        $this->assertTrue(method_exists($factory, 'configureSsl'));
-    }
-
     public function testCreateConnectionWithSslOptions(): void
     {
         $factory = new AmqpFactory();
